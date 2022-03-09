bc5f7c668962267fad64ab925214e6af8df87f59
Fábio Teixeira <fabio@fabio.vet.br>
2022-03-09 11:20:58 -0300

    Incluindo botão carregar mais

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -13,7 +13,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String? _search;
-  final int _offset = 0;
+  int _offset = 0;
+  final int _limit = 3;
 
   Future<Map> _getGifs() async {
     http.Response response;
@@ -26,7 +27,7 @@ class _HomePageState extends State<HomePage> {
     } else {
       response = await http.get(
         Uri.parse(
-            "https://api.giphy.com/v1/gifs/search?api_key=t1EaWQ7sIWXuEF5fje8mG4GuzFxsU2Ws&q=$_search&limit=20&offset=$_offset&rating=g&lang=en"),
+            "https://api.giphy.com/v1/gifs/search?api_key=t1EaWQ7sIWXuEF5fje8mG4GuzFxsU2Ws&q=$_search&limit=$_limit&offset=$_offset&rating=g&lang=en"),
       );
     }
     return json.decode(response.body);
@@ -52,19 +53,29 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.black,
       body: Column(children: [
-        const Padding(
-          padding: EdgeInsets.all(10.0),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
           child: TextField(
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               labelText: "Pesquise aqui!",
               labelStyle: TextStyle(color: Colors.white),
               border: OutlineInputBorder(),
             ),
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.white,
               fontSize: 18,
             ),
             textAlign: TextAlign.center,
+            onSubmitted: (text) {
+              setState(() {
+                if (text.isNotEmpty) {
+                  _search = text;
+                  _offset = 0;
+                } else {
+                  _search = null;
+                }
+              });
+            },
           ),
         ),
         Expanded(
@@ -97,6 +108,14 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
   Widget _createGitTable(BuildContext context, AsyncSnapshot snapshot) {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
@@ -105,15 +124,44 @@ class _HomePageState extends State<HomePage> {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
-      itemCount: snapshot.data["data"].length,
+      itemCount: _getCount(snapshot.data["data"]),
       itemBuilder: (context, index) {
-        return GestureDetector(
-          child: Image.network(
-            snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-            height: 300,
-            fit: BoxFit.cover,
-          ),
-        );
+        if (_search == null || index < snapshot.data["data"].length) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              height: 300,
+              fit: BoxFit.cover,
+            ),
+          );
+        } else {
+          return Container(
+            child: GestureDetector(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 70,
+                  ),
+                  Text(
+                    "Carregar mais...",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+              onTap: () {
+                setState(() {
+                  _offset += _limit;
+                });
+              },
+            ),
+          );
+        }
       },
     );
   }
